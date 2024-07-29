af26c352cb3cc2938bc4c8415e60f859f6d99e90
VoHuan <95951045+VoHuan@users.noreply.github.com>
2024-07-29 15:04:04 +0700

    add DB password

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Sequelize, Op } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, null, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
   pool: {
